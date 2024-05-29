5839803faee662161cdbe31da1fea993c68cf3ed
duttastu <148550607+duttastu@users.noreply.github.com>
2024-05-29 14:29:13 +0530

    Add files via upload

---
+++ add/add/Class1.cs
@@ -0,0 +1,28 @@
+﻿namespace add
+{
+    public class Class1
+    {
+        public int Addition { get; set; }
+        public int addd(int a, int b)
+        {
+            return a + b;
+        }
+        public int Multiply { get; set; }
+        public int mul(int a, int b)
+        {
+            return a * b;
+        }
+        public int Divide { get; set; }
+        public int div(int a, int b)
+        {
+            return a / b;
+        }
+        public int Sub { get; set; }
+        public int sub(int a, int b)
+        {
+            return a - b;
+        }
+
+
+    }
+}
