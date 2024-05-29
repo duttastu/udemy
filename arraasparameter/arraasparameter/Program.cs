95fd3df0cedc63aaac7ee4e5eaf32622c05c465a
duttastu <148550607+duttastu@users.noreply.github.com>
2024-05-29 14:30:00 +0530

    Add files via upload

---
+++ arraasparameter/arraasparameter/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arraasparameter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] studentsGrades = new int[] {23,54,12,44,22,3,16 };
+            double averageResult = GetAverage(studentsGrades);    
+
+            foreach (int grades in studentsGrades)
+            {
+                Console.WriteLine(" {0} ",grades);
+            }
+
+            Console.WriteLine("The average is: {0}" ,averageResult);
+            Console.ReadKey();
+        }
+        static double GetAverage(int[] gradesArray)
+        {
+            int size=gradesArray.Length;
+            int sum=0;
+            double average;
+            for (int i=0; i<size; i++)
+            {
+                sum+= gradesArray[i];
+            }
+            average=(double)sum/size;
+            return average;
+        }
+    }
+}
